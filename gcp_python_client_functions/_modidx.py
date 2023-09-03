74b10263e0c4dc26fe61c926ec88973c994aed14
Daniela Zamora <danizhorta24@gmail.com>
2023-09-03 15:13:22 -0600

    Added BigQuery module; Wrote `query_to_df` function; Added tests; Update index;

--- gcp_python_client_functions/_modidx.py
+++ gcp_python_client_functions/_modidx.py
@@ -5,5 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://datasciencedani.github.io',
                 'git_url': 'https://github.com/datasciencedani/gcp-python-client-functions',
                 'lib_path': 'gcp_python_client_functions'},
-  'syms': { 'gcp_python_client_functions.core': { 'gcp_python_client_functions.core.foo': ( 'core.html#foo',
+  'syms': { 'gcp_python_client_functions.bigquery': { 'gcp_python_client_functions.bigquery.query_to_df': ( 'bigquery.html#query_to_df',
+                                                                                                            'gcp_python_client_functions/bigquery.py')},
+            'gcp_python_client_functions.core': { 'gcp_python_client_functions.core.foo': ( 'bigquery.html#foo',
                                                                                             'gcp_python_client_functions/core.py')}}}
